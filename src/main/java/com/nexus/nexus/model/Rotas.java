3a1ba58cb3bb5752752766f697d0e3ec390d54d7
JohnnyFMartins <geral.johnny@gmail.com>
2025-09-30 20:41:42 -0300

    x

--- src/main/java/com/nexus/nexus/model/Rotas.java
+++ src/main/java/com/nexus/nexus/model/Rotas.java
@@ -1,24 +1,18 @@
 package com.nexus.nexus.model;
-
-<<<<<<< HEAD
-=======
-import java.util.List;
-import jakarta.persistence.ElementCollection; 
->>>>>>> origin/main
+ 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
-
-
+ 
+ 
 @Data
 @Entity
 @Table(name = "Rotas")
 public class Rotas {
-<<<<<<< HEAD
-	
+ 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -32,18 +26,4 @@ public class Rotas {
 	private int tempoEstimado;
 	
 }
-=======
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    @ElementCollection 
-    private List<String> pontosPassagem;
-    
-    private double distanciaTotalRota;
-    
-    private double tempoEstimado;
-    
-}
->>>>>>> origin/main
+ 
